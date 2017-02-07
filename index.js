ad4602e336c48c1b11e18c0881cfde95ea9b3333
chaohangz <chaohangz@gmail.com>
2017-02-07 17:34:00 +0800

    添加视图模板，实现注册登录登出

--- index.js
+++ index.js
@@ -19,23 +19,44 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 // session 中间件
 app.use(session({
-	name: config.session.key, // 设置cookie中保存session id的字段名称
-	secret: config.session.secret, // 通过设置secret来计算hash值并放在cookie中，使产生的signedCookie防篡改
-	cookie: {
-		maxAge: config.session.maxAge // 过期时间，过期后cookie中的session id 自动删除
-	},
-	store: new MongoStore({
-		url: config.mongodb  // mongodb地址
-	})
+  name: config.session.key, // 设置cookie中保存session id的字段名称
+  secret: config.session.secret, // 通过设置secret来计算hash值并放在cookie中，使产生的signedCookie防篡改
+  cookie: {
+    maxAge: config.session.maxAge // 过期时间，过期后cookie中的session id 自动删除
+  },
+  store: new MongoStore({
+    url: config.mongodb  // mongodb地址
+  })
 }))
 
 // flash 中间件，用来显示通知
 app.use(flash())
+
+// 处理表单及文件上传的中间件
+app.use(require('express-formidable')({
+  uploadDir: path.join(__dirname, 'public/img'),  // 上传文件目录
+  keepExtensions: true //保留上传的文件后缀
+}))
+
+// 设置模板全局常量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+// 添加模板必需的三个全局变量
+//
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user
+  res.locals.success = req.flash('success').toString()
+  res.locals.error = req.flash('error').toString()
+  next()
+})
 
 // 路由
 routes(app)
 
 // 监听端口，启动程序
 app.listen(config.port, function () {
-	console.log('${pak.name} listening on port ${config.port}')
+  console.log('${pak.name} listening on port ${config.port}')
 })
--- middlewares/check.js
+++ middlewares/check.js
@@ -1,17 +1,17 @@
 module.exports = {
-	checkLogin: function checkLogin(req, res, next) {
-		if (!req.session.user) {
-			req.flash('error', '未登录')
-			return res.redirect('/signin')
-		}
-		next()
-	},
+  checkLogin: function checkLogin(req, res, next) {
+    if (!req.session.user) {
+      req.flash('error', '未登录')
+      return res.redirect('/signin')   // 重定向至登录页面
+    }
+    next()
+  },
 
-	checkNotLogin: function checkNotLogin(req, res, next) {
-		if (req.session.user) {
-			req.flash('error', '已登录')
-			return res.redirect('back')
-		}
-		next()
-	}
+  checkNotLogin: function checkNotLogin(req, res, next) {
+    if (req.session.user) {
+      req.flash('error', '已登录')
+      return res.redirect('back')    // 如果用户已登录，则跳转到之前页面
+    }
+    next()
+  }
 }
